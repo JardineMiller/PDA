2cb87c1f2da191cc99d873b360099f8b2e7ab702
Jardine <jardine.miller@gmail.com>
2017-12-03 16:51:09 +0000

    Testing tasks completed

--- Coding exercises/Static_and_Dynamic-Task_A/testing_task_1.rb
+++ Coding exercises/Static_and_Dynamic-Task_A/testing_task_1.rb
@@ -12,6 +12,9 @@ def func1 val
   return false
   end
 end
+# 1. Parameter needs to be in brackets - without it, it will return an error.
+# 2. Comparitive operators need to have a double equals (==)
+
   
 dif max a b
   if a > b
@@ -21,7 +24,11 @@ dif max a b
   end 
 end 
 end 
-  
+# 1. Declaration of the function has been spelt incorrectly - should be 'def'
+# 2. a & b parametres need to be contained in bracket and separated by a comma
+# 3. There is 1 too many end statements
+
+
 def looper 
   for i in 1..10
   puts i
@@ -35,6 +42,7 @@ if looper == 10
 else
   puts "looper failed"
   failures = failures + 1
+# Must close the if statement with an 'end'
  
   
 if func1(3) == false
@@ -43,6 +51,7 @@ else
   puts "func1(3) failed"
   failures = failures + 1
 end 
+# This will return an error as a result of the funct1 function not being set up correctly on line 8
  
   
 if max(100,1) == 100 
@@ -51,6 +60,7 @@ else
   puts "func1(3) failed"
   failrues = failures + 1
 end
+# Failures has been mispelt
 
   
 if failures 
@@ -58,5 +68,8 @@ if failures
 else
   puts "Test Passed"
 end
+# This condition will always be met. Failures has been set to 0 on line 38.
+# The conditional statement 'if failures' is purely checking that the variable
+# 'failures' exists, which it does, even if its value is 0.
 
 
--- Coding exercises/Static_and_Dynamic-Task_A/testing_task_2.rb
+++ Coding exercises/Static_and_Dynamic-Task_A/testing_task_2.rb
@@ -3,56 +3,57 @@
 # Carry out dynamic testing on the code below.
 # Correct the errors below that you spotted in task 1.
 
-def func1 val 
-  if val = 1
-  return true
+def func1(val)
+  if val == 1
+    return true
   else
-  return false
+    return false
   end
 end
-  
-dif max a b
+
+def max(a, b)
   if a > b
-      return a 
+    return a 
   else
-  b
+    b
   end 
 end 
-end 
-  
+
 def looper 
   for i in 1..10
-  puts i
+    puts i
   end
+  return i
 end
- 
+
 failures = 0 
- 
+
 if looper == 10 
   puts "looper passed"
 else
   puts "looper failed"
   failures = failures + 1
- 
-  
-if func1(3) == false
-  puts "func1(3) passed"
-else
-  puts "func1(3) failed"
-  failures = failures + 1
-end 
- 
-  
-if max(100,1) == 100 
-  puts "max(100,1) passed"
-else
-  puts "func1(3) failed"
-  failrues = failures + 1
 end
 
   
-if failures 
-  puts "Test Failed"
-else
-  puts "Test Passed"
-end
+  if func1(3) == false
+    puts "func1(3) passed"
+  else
+    puts "func1(3) failed"
+    failures = failures + 1
+  end 
+
+  
+  if max(100,1) == 100 
+    puts "max(100,1) passed"
+  else
+    puts "func1(3) failed"
+    failures = failures + 1
+  end
+
+  
+  if failures > 1
+    puts "Test Failed"
+  else
+    puts "Test Passed"
+  end
